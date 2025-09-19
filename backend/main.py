cf133423f67f978be01dbb6b4cf4a18775498588
johnnytan5 <108878474+johnnytan5@users.noreply.github.com>
2025-09-19 23:58:20 +0800

    added basic twilio call features

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from routers import phone
 
 app = FastAPI()
 
@@ -13,6 +14,7 @@ app.add_middleware(
 )
 
 # Include all routers
+app.include_router(phone.router)
 
 
 @app.get("/") 
